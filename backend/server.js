c4e8883d6d212fb9ca657de9968c13a9f69a63cf
Dev-hashh <devk07322@gmail.com>
2026-06-28 22:27:02 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,17 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      process.env.CLIENT_URL,
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
